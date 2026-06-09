1c1194c2ab756d08f373b6daceed625596db791c
gigayulianti <yulianti2@gmail.com>
2026-06-09 11:23:33 +0700

    commit 6

--- BinarySearchtree.cpp
+++ BinarySearchtree.cpp
@@ -58,4 +58,17 @@ public:
     }
 
     // this function searches the current node of the specified node
-    
+    // as well as the current node of its parent
+    void search(string element, node *&parent, node *&currentnode)
+    {
+        currentnode = root;
+        parent = NULL;
+
+        while (currentnode != NULL &&)
+            (currentnode->info != element)
+        {
+            parent = currentnode;
+
+            if (element < currentnode->info)
+                currentnode = currentnode->leftchild;
+            
